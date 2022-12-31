c394292054ad4224c8eba357d87ebba1c1ba977a
Mike Williams <mike@buildlab.co>
2022-12-30 23:13:23 -0500

    init

---
+++ apps/blog/tailwind.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}', '../../packages/ui/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
---
+++ packages/ui/Header.svelte
@@ -0,0 +1,3 @@
+<div class="bg-blue-100 w-full">
+Nav
+</div>
